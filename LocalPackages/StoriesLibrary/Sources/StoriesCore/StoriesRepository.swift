74e875306eecd5163fbc9dafa6b3ae4e093b1d14
Gautier BILLARD <gautier.billard@PARMPYL42K1.ratpsmart.local>
2025-05-22 14:18:34 +0200

    Set-up repository with basic apis.

--- LocalPackages/StoriesLibrary/Sources/StoriesCore/StoriesRepository.swift
+++ LocalPackages/StoriesLibrary/Sources/StoriesCore/StoriesRepository.swift
@@ -1,8 +1,25 @@
-//
-//  File.swift
-//  StoriesLibrary
-//
-//  Created by Gautier BILLARD on 22/05/2025.
-//
-
 import Foundation
+
+package actor StoriesRepository {
+
+    package enum RepositoryError: Error {
+        case serverError
+    }
+
+    private let service: StoriesService
+    private var cachedStories: [StoryModel] = []
+
+    public init(service: StoriesService) {
+        self.service = service
+    }
+
+    // MARK: API
+
+    package func getStories() async throws(RepositoryError) -> [StoryModel] {
+        []
+    }
+
+    package func updateStoryLiked(liked: Bool, storyId: UUID, pageId: UUID) async throws(RepositoryError) {
+        // ..
+    }
+}
