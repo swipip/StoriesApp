d1f7dcce053287697ec9a000c270cba46db9c219
Gautier BILLARD <gautier.billard@PARMPYL42K1.ratpsmart.local>
2025-05-22 14:33:02 +0200

    Set-up service with random data.

--- LocalPackages/StoriesLibrary/Sources/Stories/Service/Service+mock.swift
+++ LocalPackages/StoriesLibrary/Sources/Stories/Service/Service+mock.swift
@@ -24,7 +24,48 @@ private actor MockService {
     }
 
     func loadStories() async -> [StoryModel] {
-        []
+        var stories = [StoryModel]()
+        let numberOfStories = Int.random(in: 3 ... 8)
+        for index in 0 ..< numberOfStories {
+            var pages = [PageModel]()
+
+            let numberOfPages = Int.random(in: 1 ... 10)
+            let user = randomUser(index: index)
+
+            for pageIndex in 0 ..< numberOfPages {
+                let asset = AssetModel(id: UUID(), mediaUrl: randomUrl())
+                pages.append(PageModel(id: UUID(), index: pageIndex, asset: asset, displayDuration: 10, liked: false, viewed: true))
+            }
+
+            stories.append(StoryModel(id: UUID(), user: user, index: index, viewed: false, postedAt: .distantPast, pages: pages))
+        }
+
+        cachedStories = stories
+
+        return stories
     }
 
+    private func randomUrl() -> URL {
+        URL(string: "https://picsum.photos/1200/2400?random=\(UUID().uuidString)")!
+    }
+
+    private func randomUser(index: Int) -> UserModel {
+        UserModel(
+            id: UUID(),
+            username: "user\(index)",
+            profileImageURL: randomUserUrl(),
+            isApproved: .random(),
+            communication: Bool.random() ? nil : "partenariat commercial avec **Adidas**",
+            postedSince: "\((8 ... 22).randomElement() ?? 2) h")
+    }
+
+    private func randomUserUrl() -> URL {
+        let type = ["men", "women"].randomElement() ?? "men"
+        let index = (0 ..< 99).randomElement() ?? 1
+
+        let url =  URL(string: "https://randomuser.me/api/portraits/\(type)/\(index).jpg")!
+        print(url)
+
+        return url
+    }
 }
